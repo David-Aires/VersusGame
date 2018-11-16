aee7af1b1b48e494b203e6a9ef886df2a9a1f42a
aires-david <david.airespimentel@gmail.com>
2018-11-16 15:36:57 +0100

    changement du type de Tiledmap et music pour facilité la manipulation

--- src/main/ModelWindow.java
+++ src/main/ModelWindow.java
@@ -3,59 +3,111 @@
  */
 package main;
 
-
+//Suite à un problème technique, nous avons dû refaire un repository
+//https://github.com/David-Aires/VersusGame.git
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-import org.newdawn.slick.Music;
-import org.newdawn.slick.tiled.TiledMap;
-
 /**
  * @author Aires David
- *
+ * 
  */
 
 public class ModelWindow {
 
 
 	
-		
-		private TiledMap tiledMap;
-		private Music music;
-		private int screenWidth;
-		private int screenHeight;
-		private Dimension dimScreen;
-		private Toolkit tk;
-		
-		
-		public ModelWindow(TiledMap tiledMap, Music music ) {
-			this.tiledMap = tiledMap;
-			this.music = music;
-		}
+	
+	/**
+	 * The TiledMap
+	 */
+	private String tiledMap;
+	/**
+	 * The Music
+	 */
+	private String music;
+	/**
+	 * the width of the screen
+	 */
+	private  int screenWidth;
+	/**
+	 * the height of the screen
+	 */
+	private  int screenHeight = 0;
+	/**
+	 * The dimension of the screen
+	 */
+	private Dimension dimScreen;
+	/**
+	 * Tools of toolkit library
+	 */
+	private Toolkit tk;
+	
+	
+	/**
+	 * This constructor create a map using the tiledmap and the music
+	 * @param tiledmap
+	 * @param music
+	 */
+	public ModelWindow(String tiledmap, String music ) {
+		this.tiledMap = tiledmap;
+		this.music = music;
+		getscreenSize();
+	}
+	
 
-		public TiledMap getTiledMap() {
-			return tiledMap;
-		}
+	/**
+	 * @return the tiledmap
+	 */
+	public String getTiledMap() {
+		return tiledMap;
+	}
+
+	/**
+	 * @return the music
+	 */
+	public String getMusic() {
+		return music;
+	}
+	
+	/**
+	 * @return the width and the height of the system screen
+	 */
+	public void getscreenSize() {
+		tk = Toolkit.getDefaultToolkit();
+		dimScreen = tk.getScreenSize();
+		this.screenWidth = dimScreen.width;
+		this.screenHeight = dimScreen.height;
+		
+		
+	}
+
+	/**
+	 * @return the screenWidth
+	 */
+	public int getScreenWidth() {
+		return screenWidth;
+	}
+
+	/**
+	 * @return the screenHeight
+	 */
+	public int getScreenHeight() {
+		return screenHeight;
+	}
+
+	/**
+	 * @return the dimScreen
+	 */
+	public Dimension getDimScreen() {
+		return dimScreen;
+	}
+	
+	
 
 		
-		public Music getMusic() {
-			return music;
-		}
-
-		public int[] getscreenSize() {
-			tk = Toolkit.getDefaultToolkit();
-			dimScreen = tk.getScreenSize();
-			screenWidth = dimScreen.width;
-			screenHeight = dimScreen.height;
-			int[] sizeScreen = {screenWidth,screenHeight};
-			return sizeScreen;
-			
-		}
 		
-		public int getscreenWidth() {
-			return this.screenWidth;
-		}
 		
 		
 		
