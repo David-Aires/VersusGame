6bc8273e0c5ef1df6f23c159d5cd085d86d3b7f1
aires-david <david.airespimentel@gmail.com>
2018-11-17 11:34:48 +0100

    Création du menu

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -1,7 +1,110 @@
 package main;
 
-public class MainWindow {
-	public static void main(String[] args) {
-		System.out.println("test");
+import java.awt.Font;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainWindow extends BasicGameState implements CONSTANTS {
+	  public static final int ID = 1;
+	  private StateBasedGame game;
+	  private Image backgroundMap;
+	  private Music backgroudMusic;
+	  private int playersChoice = 0;
+	    private static final int NOCHOICES = 3;
+	    private static final int START = 0;
+	    private static final int INFO = 1;
+	    private static final int QUIT = 2;
+	    private String[] playersOptions = new String[NOCHOICES];
+	    private boolean exit = false;
+	    private Font font;
+	    private TrueTypeFont playersOptionsTTF, foo;
+	    private Color notChosen = new Color(153, 204, 255);
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		ModelWindow menu = new ModelWindow(menuImage,musicMenu);
+		this.game = game;
+		this.backgroundMap = new Image(menu.getTiledMap());
+		this.backgroudMusic = new Music(menu.getMusic());
+		font = new Font("Verdana", Font.BOLD, 40);
+        playersOptionsTTF = new TrueTypeFont(font, true);
+        font = new Font ("Verdana", Font.PLAIN, 20);
+        foo = new TrueTypeFont(font, true);
+        playersOptions[0] = "Start";
+        playersOptions[1] = "Info";
+        playersOptions[2] = "Quit";
+        backgroudMusic.loop();
+       
+		
 	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		backgroundMap.draw(0, 0, container.getWidth(), container.getHeight());
+		 renderPlayersOptions();
+	        if (exit) {
+	            container.exit();
+	}
+		
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		Input input = container.getInput();
+		container.setFullscreen(fullscreen);
+        if (input.isKeyPressed(Input.KEY_DOWN)) {
+            if (playersChoice == (NOCHOICES - 1)) {
+                playersChoice = 0;
+            } else {
+                playersChoice++;
+            }
+        }
+        if (input.isKeyPressed(Input.KEY_UP)) {
+            if (playersChoice == 0) {
+                playersChoice = NOCHOICES - 1;
+            } else {
+                playersChoice--;
+            }
+        }
+        if (input.isKeyPressed(Input.KEY_ENTER)) {
+            switch (playersChoice) {
+                case QUIT:
+                    exit = true;
+                    break;
+                
+                case START:
+                	backgroudMusic.stop();
+                	 game.enterState(MainMap.ID);
+                	 break;
+            }
+}
+		
+	}
+	
+	private void renderPlayersOptions() {
+        for (int i = 0; i < NOCHOICES; i++) {
+            if (playersChoice == i) {
+                playersOptionsTTF.drawString(100, i * 50 + 200, playersOptions[i]);
+            } else {
+                playersOptionsTTF.drawString(100, i * 50 + 200, playersOptions[i], notChosen);
+            }
+        }
+    }
+
+	
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return ID;
+	}
+	
 }
