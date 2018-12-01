d7a2a8436e2b7396e0006be96b17bb59333573f9
QL <q.lebrun@students.ephec.be>
2018-12-01 17:08:15 +0100

    add commentaires & chgmt position de départ joueurs car chgt terrain

--- src/versus/model/PlayerModel.java
+++ src/versus/model/PlayerModel.java
@@ -8,7 +8,7 @@ import java.util.Observable;
 
 
 /**
- * @author Aires David
+ * @author Aires David, Quentin Lebrun
  *
  */
 public class PlayerModel extends Observable {
@@ -18,13 +18,14 @@ public class PlayerModel extends Observable {
 	public PlayerModel() {
 		plocalModel= new CharacterModel();
 		plocalModel.setX(0);
-		plocalModel.setY(6);
+		plocalModel.setY(5);
 		
 		enemyModel= new CharacterModel();
-		enemyModel.setX(14);
-		enemyModel.setY(6);
+		enemyModel.setX(10);
+		enemyModel.setY(5);
 	}
 	
+	// change the position in x,y and the boolean moving
 	public void mouvementsLocal(int x,int y,boolean moving) {
 		if(x!=plocalModel.getX() || y!=plocalModel.getY()) {
 			plocalModel.mouvement(x, y,moving);
@@ -43,29 +44,32 @@ public class PlayerModel extends Observable {
 		return str;
 		
 	}
-	
+	// get position x for player local Model
 	public int getLX() {
 		return plocalModel.getX();
 	}
-	
+	// get position y for player local Model
 	public int getLY() {
 		return plocalModel.getY();
 	}
 	
+	// get the state of moving of  player local Model
 	public boolean getLMoving() {
 		return plocalModel.isMoving();
 	}
 
 	
-	
+	// get position x for enemy Model
 	public int getRX() {
 		return enemyModel.getX();
 	}
 	
+	// get position y for enemy Model
 	public int getRY() {
 		return enemyModel.getY();
 	}
 	
+	// get the state of moving of  enemy Model
 	public boolean getRMoving() {
 		return enemyModel.isMoving();
 	}
