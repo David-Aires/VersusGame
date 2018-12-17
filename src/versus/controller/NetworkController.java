708e75b8cf94ae339a777a79cf8dba1140220ee8
QL <q.lebrun@students.ephec.be>
2018-12-17 20:43:25 +0100

    version plus simple pour l'ajustement

--- src/versus/controller/NetworkController.java
+++ src/versus/controller/NetworkController.java
@@ -119,78 +119,13 @@ public class NetworkController  {
 		
 	// return the good x for the other player in the network
 		public int changeLocalXForNetwork(){
-			if(player.getLX()==0){
-				return 10;
-			}
-			if(player.getLX()==1){
-				return 9;
-			}
-			if(player.getLX()==2){
-				return 8;
-			}
-			if(player.getLX()==3){
-				return 7;
-			}
-			if(player.getLX()==4){
-				return 6;
-			}
-			if(player.getLX()==5){
-				return 5;
-			}
-			if(player.getLX()==6){
-				return 4;
-			}
-			if(player.getLX()==7){
-				return 3;
-			}
-			if(player.getLX()==8){
-				return 2;
-			}
-			if(player.getLX()==9){
-				return 1;
-			}
-			if(player.getLX()==10){
-				return 0;
-			}
-			return 0;
+			return 10-player.getLX();
 		}
 		
 		// return the good y for the other player in the network
 		public int changeLocalYForNetwork(){
-			if(player.getLY()==0){
-				return 10;
-			}
-			if(player.getLY()==1){
-				return 9;
-			}
-			if(player.getLY()==2){
-				return 8;
-			}
-			if(player.getLY()==3){
-				return 7;
-			}
-			if(player.getLY()==4){
-				return 6;
-			}
-			if(player.getLY()==5){
-				return 5;
-			}
-			if(player.getLY()==6){
-				return 4;
-			}
-			if(player.getLY()==7){
-				return 3;
-			}
-			if(player.getLY()==8){
-				return 2;
-			}
-			if(player.getLY()==9){
-				return 1;
-			}
-			if(player.getLY()==10){
-				return 0;
-			}
-			return 0;
+		
+			return 10-player.getLY();
 		}
 		
 		
